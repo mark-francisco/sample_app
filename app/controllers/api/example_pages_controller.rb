3f27982cf9abe142f6145b202aca8baa7725d2c1
Mark Francisco <mfrancisco581@gmail.com>
2021-02-23 00:57:05 -0600

    2/22/2021 exercise: Add routes, controller methods, and view file to sample_app

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,27 @@
 class Api::ExamplePagesController < ApplicationController
+
+  def mark_route_action
+    render json: { message: "this is what shows up when you go to the route: /api/mark_route" }
+  end
+
+  def dog_route_action
+    render json: { message: "boxers, beagles, golden retrievers. you are at this route: /api/dog_route" }
+  end
+
+  def cat_route_action
+    render json: { message: "meow. are cats tigers?" }
+  end
+
+  def hey_html_action
+    # render HTML?
+    render inline: "<h1>Hello, world!</h1><p>how are you?</p>"
+  end
+
+  def using_external_view_action
+    @message = "this is a message from the new json.jb file",
+    @date = Date.today
+    
+    render "use_the_view.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    # VERB "/[name of route]" => "[controller]_controller#[intended action]"
+    get "/mark_route" => "example_pages#mark_route_action"
+    get "/dog_route" => "example_pages#dog_route_action"
+    get "/cat_route" => "example_pages#cat_route_action"
+
+    get "/hey_route" => "example_pages#hey_html_action"
+
+    get "/what_a_view" => "example_pages#using_external_view_action"
+  end
 end
+
+# • Create an api with THREE paths that return different messages!
+# • Bonus: Try to render more complex information (arrays with hashes within arrays, etc.)
+# • Bonus: Try to render something other than JSON! (Rails can render XML or HTML) (edited) 
+# Rewrite your controller actions to use jb views instead!
